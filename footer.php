cfba49e5d42f0f10185cab14a68f6639a635707f
Matt Peacock <matt@miltonbayer.com>
2018-03-05 12:16:48 +0000

    Closing PHP on new line
    Signed-off-by: Matt Peacock <matt@miltonbayer.com>

--- footer.php
+++ footer.php
@@ -15,10 +15,12 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'scratch' ) ); ?>"><?php
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'scratch' ) ); ?>">
+			<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Proudly powered by %s', 'scratch' ), 'WordPress' );
-			?></a>
+			?>
+			</a>
 			<span class="sep"> | </span>
 			<?php
 				/* translators: 1: Theme name, 2: Theme author. */
