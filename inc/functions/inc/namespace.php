ac1a395bcdc8ab6bf60d95f30d9ca0d32a9ed737
Matt Peacock <matt@miltonbayer.com>
2018-03-05 12:31:43 +0000

    Tabs instead of spaces WPCS
    Signed-off-by: Matt Peacock <matt@miltonbayer.com>

--- inc/functions/inc/namespace.php
+++ inc/functions/inc/namespace.php
@@ -18,47 +18,49 @@ namespace WPA_Scratch\Functions;
  */
 function setup() {
 	/*
- 	* Make theme available for translation.
- 	* Translations can be filed in the /languages/ directory.
- 	* If you're building a theme based on scratch, use a find and replace
- 	* to change 'scratch' to the name of your theme in all the template files.
- 	*/
+	* Make theme available for translation.
+	* Translations can be filed in the /languages/ directory.
+	* If you're building a theme based on scratch, use a find and replace
+	* to change 'scratch' to the name of your theme in all the template files.
+	*/
 	load_theme_textdomain( 'scratch', get_template_directory() . '/languages' );
 
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
 
 	/*
- 	* Let WordPress manage the document title.
- 	* By adding theme support, we declare that this theme does not use a
- 	* hard-coded <title> tag in the document head, and expect WordPress to
- 	* provide it for us.
- 	*/
+	* Let WordPress manage the document title.
+	* By adding theme support, we declare that this theme does not use a
+	* hard-coded <title> tag in the document head, and expect WordPress to
+	* provide it for us.
+	*/
 	add_theme_support( 'title-tag' );
 
 	/*
- 	* Enable support for Post Thumbnails on posts and pages.
- 	*
- 	* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
- 	*/
+	* Enable support for Post Thumbnails on posts and pages.
+	*
+	* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	*/
 	add_theme_support( 'post-thumbnails' );
 
 	// This theme uses wp_nav_menu() in three locations.
-	register_nav_menus( [
-		'menu-1' => esc_html__( 'Main Menu', 'scratch' ),
+	register_nav_menus(
+		[
+			'menu-1' => esc_html__( 'Main Menu', 'scratch' ),
 		]
 	);
 
 	/*
- 	* Switch default core markup for search form, comment form, and comments
- 	* to output valid HTML5.
- 	*/
-	add_theme_support( 'html5', [
-		'search-form',
-		'comment-form',
-		'comment-list',
-		'gallery',
-		'caption',
+	* Switch default core markup for search form, comment form, and comments
+	* to output valid HTML5.
+	*/
+	add_theme_support( 'html5',
+		[
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
 		]
 	);
 
